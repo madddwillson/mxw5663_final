93d5f9b998603abc45e7dbe2959859f561d89cb8
Madeleine Willson <112000029+madddwillson@users.noreply.github.com>
2024-04-30 18:13:25 -0400

    Update mxw5663.sh
    again

--- mxw5663.sh
+++ mxw5663.sh
@@ -2,24 +2,27 @@
 
 
 # Load necessary modules
-module load Anaconda3/2020.11
+module load Anaconda3
+
+
+# Remove the existing Conda environment 
+conda remove --name mxw5663_final --all -y
 
 
 # Create a new conda virtual environment
-conda create -n  mxw5663_final python=3.7
+conda create -n mxw5663_final python=3.7 -y
 
 
 # Activate the new virtual environment
-source activate mxw5663_final
+conda activate mxw5663_final
+
 
 # Install sparktorch 
-pip install git+https://github.com/dmmiller612/sparktorch.git
+pip install git+https://github.com/dmmiller612/sparktorch.git torch
 
 
 # Download the dataset
 wget -O mnist_train.csv https://raw.githubusercontent.com/dmmiller612/sparktorch/master/examples/mnist_train.csv
-
-
 # Train the deep CNN using sparktorch
 python train_cnn.py --num-executors 4 --executor-cores 4 --executor-memory 8G
 
