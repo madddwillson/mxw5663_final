96846f8c6fcb11d22116e304ee20ab82f5d0c877
Madeleine Willson <112000029+madddwillson@users.noreply.github.com>
2024-04-30 17:56:27 -0400

    Update mxw5663.sh
    updated

--- mxw5663.sh
+++ mxw5663.sh
@@ -6,7 +6,7 @@ module load Anaconda3/2020.11
 
 
 # Create a new conda virtual environment
-conda create -n  mxw5663_final python=3.8
+conda create -n  mxw5663_final python=3.7
 
 
 # Activate the new virtual environment
@@ -17,16 +17,12 @@ pip install git+https://github.com/dmmiller612/sparktorch.git
 
 
 # Download the dataset
-wget -O mnist_train.csv https://raw.githubusercontent.com/dmmiller612/sparktorc$
+wget -O mnist_train.csv https://raw.githubusercontent.com/dmmiller612/sparktorch/master/examples/mnist_train.csv
 
 
 # Train the deep CNN using sparktorch
 python train_cnn.py --num-executors 4 --executor-cores 4 --executor-memory 8G
 
 
-# Provide the code in a GitHub repository
-# Create a GitHub repository and push the necessary code including the modified$
-
-
 # Provide the GitHub repository link for the git clone
-git clone https://github.com/madddwillson/mxw5663_Final.git
+git clone https://github.com/madddwillson/mxw5663_final.git
